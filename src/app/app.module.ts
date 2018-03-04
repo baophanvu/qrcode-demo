0d9131ad5a0ffb1dea3142784cc98df770bc495d
Khoa Hoang <khoahoang06.tech@gmail.com>
2018-03-04 11:30:13 +0700

    ngx-xing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgQrScannerModule } from 'angular2-qrscanner';
+import { NgxZxingModule } from '@zxing/ngx-scanner';
 
 import { AppComponent } from './app.component';
 
@@ -11,7 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    NgQrScannerModule
+    NgxZxingModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]

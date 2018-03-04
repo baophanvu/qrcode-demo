3457934629327e1ee0696dfd3604c26dcace454a
Khoa Hoang <khoahoang06.tech@gmail.com>
2018-03-04 09:43:14 +0700

    scan QR code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgQrScannerModule } from 'angular2-qrscanner';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgQrScannerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
